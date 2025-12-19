9fa58b670c21340f86b9a395e25db929d7dbf22f
EfraimNS <efraimns300@gmail.com>
2025-12-19 00:23:53 -0300

    Primeiro commit

---
+++ src/main/java/br/com/praticandojava/projeto2/Produto.java
@@ -0,0 +1,42 @@
+package br.com.praticandojava.projeto2;
+
+public class Produto {
+
+    private String name;
+    private double price;
+
+    public Produto(String nome, double price) {
+        if (price <= 0) {
+            throw new RuntimeException("O preço do produto não pode ter valor zero ou negativo.");
+        }
+        this.name = nome;
+        this.price = price;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDetalhes() {
+        return "";
+    }
+
+
+
+
+
+
+}
